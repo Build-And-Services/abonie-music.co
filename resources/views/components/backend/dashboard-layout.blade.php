0cc6b9d6ac9ded7fde7a9eef4ea5bbafe3568f2e
Heri Setyawan <herisetyawan233@gmail.com>
2024-05-20 01:02:19 +0700

    feat: apply our feature to theme

--- resources/views/components/backend/dashboard-layout.blade.php
+++ resources/views/components/backend/dashboard-layout.blade.php
@@ -9,13 +9,28 @@
     <link rel="stylesheet" href="{{ asset('assets-dashboard/css/tailwind2.css') }}">
     @stack('stylesheet')
 </head>
-<body data-mode="light" data-sidebar-size="lg" class="group">
+<body data-mode="dark" data-sidebar-size="lg" class="group">
     <x-backend.sidebar/>
     <x-backend.navbar/>
 
     <div class="main-content group-data-[sidebar-size=sm]:ml-[70px]">
         <div class="min-h-screen page-content dark:bg-zinc-700">
             {{ $slot }}
+
+            <!-- Footer Start -->
+            <footer class="absolute bottom-0 left-0 right-0 px-5 py-5 bg-white border-t footer border-gray-50 dark:bg-zinc-700 dark:border-zinc-600 dark:text-gray-200">
+                <div class="grid grid-cols-2 text-gray-500 dark:text-zinc-100">
+                    <div class="grow">
+                        &copy;
+                        <script>
+                            document.write(new Date().getFullYear());
+                        </script> Minia
+                    </div>
+                    <div class="hidden md:inline-block text-end">Design & Develop by <a href="https://buildandservice.tech/" class="underline text-violet-500">Build And Service</a></div>
+            
+                </div>
+            </footer>
+            <!-- end Footer -->    
         </div>
     </div>
 
