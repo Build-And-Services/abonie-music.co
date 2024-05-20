6a20f5037895d5a52b4771d455420fa72d45a5bd
Heri Setyawan <herisetyawan233@gmail.com>
2024-05-20 21:03:20 +0700

    feat: page notfound

--- resources/views/layouts/backend/dashboard-layout.blade.php
+++ resources/views/layouts/backend/dashboard-layout.blade.php
@@ -1,3 +1,5 @@
+@props(['notfound' => false])
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
 <head>
@@ -10,29 +12,35 @@
     @stack('stylesheet')
 </head>
 <body data-mode="dark" data-sidebar-size="lg" class="group">
-    <x-backend.sidebar/>
-    <x-backend.navbar/>
+    @if (!$notfound)
+        <x-backend.sidebar/>
+        <x-backend.navbar/>
 
-    <div class="main-content group-data-[sidebar-size=sm]:ml-[70px]">
-        <div class="min-h-screen page-content dark:bg-zinc-700">
-            {{ $slot }}
+        <div class="main-content group-data-[sidebar-size=sm]:ml-[70px]">
+            <div class="min-h-screen page-content dark:bg-zinc-700">
+                {{ $slot }}
 
-            <!-- Footer Start -->
-            <footer class="absolute bottom-0 left-0 right-0 px-5 py-5 bg-white border-t footer border-gray-50 dark:bg-zinc-700 dark:border-zinc-600 dark:text-gray-200">
-                <div class="grid grid-cols-2 text-gray-500 dark:text-zinc-100">
-                    <div class="grow">
-                        &copy;
-                        <script>
-                            document.write(new Date().getFullYear());
-                        </script> Minia
+                <!-- Footer Start -->
+                <footer class="absolute bottom-0 left-0 right-0 px-5 py-5 bg-white border-t footer border-gray-50 dark:bg-zinc-700 dark:border-zinc-600 dark:text-gray-200">
+                    <div class="grid grid-cols-2 text-gray-500 dark:text-zinc-100">
+                        <div class="grow">
+                            &copy;
+                            <script>
+                                document.write(new Date().getFullYear());
+                            </script> buildandservice
+                        </div>
+                        <div class="hidden md:inline-block text-end">Design & Develop by <a href="https://buildandservice.tech/" class="underline text-violet-500">Build And Service</a></div>
+                
                     </div>
-                    <div class="hidden md:inline-block text-end">Design & Develop by <a href="https://buildandservice.tech/" class="underline text-violet-500">Build And Service</a></div>
-            
-                </div>
-            </footer>
-            <!-- end Footer -->    
+                </footer>
+                <!-- end Footer -->    
+            </div>
         </div>
-    </div>
+        
+    @else
+        {{ $slot }}
+    @endif
+
 
     <script src="{{ asset('assets-dashboard/libs/@popperjs/core/umd/popper.min.js') }}"></script>
     <script src="{{ asset('assets-dashboard/libs/feather-icons/feather.min.js') }}"></script>
