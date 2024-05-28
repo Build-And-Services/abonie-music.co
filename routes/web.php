60471fffe824f0b9682118f522ef8bfa30702271
perdanaph <danangputro9@gmail.com>
2024-05-28 13:13:51 +0700

    feat: setup platfom presave & biolink

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Route;
 include __DIR__ . '/landing-page.php';
 require_once __DIR__ . '/dashboard.php';
 require_once __DIR__ . '/user.php';
+require_once __DIR__ . '/platform/biolink.php';
+require_once __DIR__ . '/platform/presave.php';
