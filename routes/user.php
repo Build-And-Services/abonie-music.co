050a71120c3b42ba3a630dd40949d265b403aa99
Faisal Bahari <fbhr1112@gmail.com>
2024-05-28 12:03:42 +0700

    feat: user filter and delete

--- routes/user.php
+++ routes/user.php
@@ -5,4 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('users')->group(function () {
     Route::get('/', [UserController::class, 'index'])->name('users.index');
+    Route::get('/{id}', [UserController::class, 'show'])->name('users.show');
+    Route::delete('/{id}', [UserController::class, 'destroy'])->name('users.destroy');
+    Route::patch('/status/{id}', [UserController::class, 'status'])->name('users.status');
 });
