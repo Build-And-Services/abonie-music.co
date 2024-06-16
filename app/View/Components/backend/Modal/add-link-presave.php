6a1ae84725adbf617883862e44d052c4c7a291e7
perdanaph <danangputro9@gmail.com>
2024-06-16 23:21:48 +0700

    fixing component class add link presave

--- app/View/Components/backend/Modal/add-link-presave.php
+++ app/View/Components/backend/Modal/add-link-presave.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class add-link-presave extends Component
+class AddLinkPresave extends Component
 {
     /**
      * Create a new component instance.
