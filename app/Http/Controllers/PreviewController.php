7a90ee9c6da24dfd5ff9d28bafec9ccc81f0e92a
perdanaph <danangputro9@gmail.com>
2024-09-30 02:22:34 +0700

    refactor: block url presave when link banned

--- app/Http/Controllers/PreviewController.php
+++ app/Http/Controllers/PreviewController.php
@@ -26,55 +26,11 @@ class PreviewController extends Controller
     public function resultPresave($slug)
     {
         $presave = Presave::with('links')->where('slug', $slug)->first();
-        $presave->viewable()->update(['count' => $presave->viewable->count + 1]);
-        return view('frontend.preview.presave', compact('presave'));
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        if ($presave->statuses->status == 1) {
+            $presave->viewable()->update(['count' => $presave->viewable->count + 1]);
+            return view('frontend.preview.presave', compact('presave'));
+        } else {
+            return view('errors.404');
+        }
     }
 }
